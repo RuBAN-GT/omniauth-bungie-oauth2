9aabf520e14490e9b5467d3284c8e1c624a7a6e6
Dmitry Ruban <dkruban@gmail.com>
2017-01-26 04:22:13 +0300

    spec

--- spec/omniauth/strategies/bungie_oauth2_spec.rb
+++ spec/omniauth/strategies/bungie_oauth2_spec.rb
@@ -6,8 +6,4 @@ describe OmniAuth::Strategies::Bungie do
   it "has a version number" do
     expect(OmniAuth::BungieOauth2::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
